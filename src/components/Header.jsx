1e4b79ad1ff8cb9d4c4e6c3414b377152f2621bd
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-12 16:15:33 +0200

    the header of the project containing the name and the chef's icon

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { claude } from "../assets/index";
+export default function Header() {
+  return (
+    <>
+      <div className="flex flex-row bg-white w-screen h-27.75 justify-center items-center" id="ex">
+        <img src={claude} alt="" className="w-12"/>
+        <h1 className="text-[#141413]">Chef Claude</h1>
+      </div>
+    </>
+  );
+}
