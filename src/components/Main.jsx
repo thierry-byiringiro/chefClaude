4623148b112d1782e0d91e3e01e062208f675a6c
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-12 17:15:03 +0200

    the main content of the page like the form

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function  Main (){
+    return (
+        <main className="mt-12 flex justify-center">
+            <form action="" className="flex gap-5">
+                <input type="text" name="" id="" placeholder="e.g. oregano" className="h-12 w-100 border rounded-md border-[#D1D5DB] pl-4"/>
+                <input type="submit" value="+ Add ingredient" className="h-12  bg-[#141413]  rounded-md text-white w-60 flex items-center"/>
+            </form>
+        </main>
+    );
+}
