3d8a5b863d8dd1fb7bd1e926a5f00aeb5a00f326
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-12 19:37:35 +0200

    added event prevent default

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,12 @@
 export default function  Main (){
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("the form now is submitted");
+    }
     return (
         <main className="mt-12 flex justify-center">
-            <form action="" className="flex gap-5">
-                <input type="text" name="" id="" placeholder="e.g. oregano" className="h-12 w-100 border rounded-md border-[#D1D5DB] pl-4"/>
+            <form action="" className="flex gap-5" onSubmit={handleSubmit}>
+                <input type="text" name="ingredient" id="" placeholder="e.g. oregano" className="h-12 w-100 border rounded-md border-[#D1D5DB] pl-4"/>
                 <input type="submit" value="+ Add ingredient" className="h-12  bg-[#141413]  rounded-md text-white w-60 flex items-center"/>
             </form>
         </main>
