708e3dff2def86e930ab8c5763e3e9cb0542dcab
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-13 17:36:34 +0200

    deleted the import of the image in the public directory as they don't need to be imported or exported

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
-import { claude } from "../assets/index";
 export default function Header() {
   return (
     <>
       <div className="flex flex-row bg-white w-screen h-27.75 justify-center items-center gap-5" id="ex">
-        <img src={claude} alt="" className="w-12"/>
+        <img src='/claude.png' alt="" className="w-12"/>
         <h1 className="text-[#141413]">Chef Claude</h1>
       </div>
     </>
