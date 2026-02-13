8caeda1df91cfb88fa7d9bbc50a878142834cfaa
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-13 21:36:56 +0200

    done conditional rendering between classes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,11 @@ export default function Main() {
   ));
   const handleSubmit = (formData) => {
     const newIngredient = formData.get("ingredient");
-    console.log(newIngredient);
-    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+    if (newIngredient.length == 0) {
+      alert("You can not add an empty text");
+    } else {
+      setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+    }
   };
 
   return (
@@ -27,23 +30,27 @@ export default function Main() {
             className="h-12  bg-[#141413]  rounded-md text-white w-60 flex items-center"
           />
         </form>
-        <section className="pr-10 flex flex-col space-y-20">
-          <div>
-            <h2 className="font-bold text-3xl">Ingredients on hand:</h2>
-            <ul className="list-disc pl-5 mt-5">{displayIngredients}</ul>
-          </div>
-          <div className="bg-[#F0EFEB] w-148.5 h-27.25 flex items-center justify-center rounded-md space-x-6">
-            <div className="flex flex-col space-y-4">
-              <h3 className="font-bold text-xl">Ready for a recipe?</h3>
-              <p className="text-[#6B7280]">
-                Generate a recipe from your list of ingredients.
-              </p>
+        {arrayOfIngredients.length > 0 && (
+          <section className="pr-10 flex flex-col space-y-20">
+            <div>
+              <h2 className="font-bold text-3xl">Ingredients on hand:</h2>
+              <ul className="list-disc pl-5 mt-5">{displayIngredients}</ul>
             </div>
-            <button className="bg-[#D17557] h-9.5 flex items-center">
-              Get a recipe
-            </button>
-          </div>
-        </section>
+            {arrayOfIngredients.length > 3 && (
+              <div className="bg-[#F0EFEB] w-148.5 h-27.25 flex items-center justify-center rounded-md space-x-6">
+                <div className="flex flex-col space-y-4">
+                  <h3 className="font-bold text-xl">Ready for a recipe?</h3>
+                  <p className="text-[#6B7280]">
+                    Generate a recipe from your list of ingredients.
+                  </p>
+                </div>
+                <button className="bg-[#D17557] h-9.5 flex items-center">
+                  Get a recipe
+                </button>
+              </div>
+            )}
+          </section>
+        )}
       </main>
     </>
   );
