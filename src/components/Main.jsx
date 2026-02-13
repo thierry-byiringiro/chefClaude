e34619bee5d8641c26dedf1ccb9f1b1b2bcc6936
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-13 17:37:16 +0200

    added a new section and change the way to get the values from the form usin the action attribute

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,19 @@
+import { useState } from "react";
 export default function Main() {
-  const arrayOfIngredients = ["carrots", "tomatoes", "oranges"];
+  const [arrayOfIngredients, setIngredients] = useState([]);
   const displayIngredients = arrayOfIngredients.map((el, id) => (
     <li key={id}>{el}</li>
   ));
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+  const handleSubmit = (formData) => {
     const newIngredient = formData.get("ingredient");
-    console.log(newIngredient)
-    arrayOfIngredients.push(newIngredient);
-    console.log(arrayOfIngredients);
+    console.log(newIngredient);
+    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
   };
 
   return (
     <>
-      <main className="mt-12 flex justify-center">
-        <form action="" className="flex gap-5" onSubmit={handleSubmit}>
+      <main className="mt-12 flex flex-col  justify-center items-center space-y-10">
+        <form action={handleSubmit} className="flex gap-5" autoComplete="off">
           <input
             type="text"
             name="ingredient"
@@ -29,8 +27,24 @@ export default function Main() {
             className="h-12  bg-[#141413]  rounded-md text-white w-60 flex items-center"
           />
         </form>
+        <section className="pr-10 flex flex-col space-y-20">
+          <div>
+            <h2 className="font-bold text-3xl">Ingredients on hand:</h2>
+            <ul className="list-disc pl-5 mt-5">{displayIngredients}</ul>
+          </div>
+          <div className="bg-[#F0EFEB] w-148.5 h-27.25 flex items-center justify-center rounded-md space-x-6">
+            <div className="flex flex-col space-y-4">
+              <h3 className="font-bold text-xl">Ready for a recipe?</h3>
+              <p className="text-[#6B7280]">
+                Generate a recipe from your list of ingredients.
+              </p>
+            </div>
+            <button className="bg-[#D17557] h-9.5 flex items-center">
+              Get a recipe
+            </button>
+          </div>
+        </section>
       </main>
-      <ul>{displayIngredients}</ul>
     </>
   );
 }
