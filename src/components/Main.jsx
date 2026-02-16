5109e27560f43e0367315c3f6d8345e5b1761ac3
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-16 19:17:09 +0200

    imported the recipe component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -62,7 +62,7 @@ export default function Main() {
           </section>
         )}
         {recipeShown && (
-          {ClaudeRecipe}
+          <ClaudeRecipe/>
         )}
       </main>
     </>
