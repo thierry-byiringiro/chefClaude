a1147afa12afb8f51f5867f0b32b53f9c8c19634
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-17 20:25:37 +0200

    integrating the ai function for generating the recipe

---
+++ src/assets/ai.js
@@ -0,0 +1,33 @@
+import { InferenceClient } from "@huggingface/inference";
+const SYSTEM_PROMPT = `
+You are an assistant that receives a list of ingredients that a user has
+ and suggests a recipe they could make with some or all of those ingredients.
+You don't need to use every ingredient they mention in your recipe.
+The recipe can include additional ingredients they didn't mention, 
+but try not to include too many extra ingredients.
+Format your response in markdown to make it easier to render to a web page
+`;
+const hf = new InferenceClient(import.meta.env.VITE_HUGGINGFACE_API_KEY);
+
+
+export async function getRecipeFromMistral(ingredientsArr) {
+  const ingredientsString = ingredientsArr.join(", ");
+  try {
+    console.log(hf)
+    const response = await hf.chatCompletion({
+      model: "mistralai/Mistral-7B-Instruct-v0.2",
+      messages: [
+        { role: "system", content: SYSTEM_PROMPT },
+        {
+          role: "user",
+          content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
+        },
+      ],
+      max_tokens: 500,
+    });
+    return response.choices[0].message.content;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
